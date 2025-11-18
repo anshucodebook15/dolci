f6708a6a4e4ba4af0be16be67c12b91461e5501f
Anshu Saxena <anshus5802@gmail.com>
2025-11-18 17:55:45 +0530

    dolci landing page done

--- src/pages/landing/landing.tsx
+++ src/pages/landing/landing.tsx
@@ -24,27 +24,8 @@ import { Link } from "react-router";
 const Hero = () => {
   return (
     <div className="pt-5 pb-20">
-      <div className="lg-contain">
-        {/* <div className="rounded-[5px] overflow-hidden h-40 ">
-          <div className="flex items-center justify-center">
-            <video
-              src={videos.dolcivd}
-              autoPlay
-              loop
-              muted
-              playsInline
-              className="w-full h-auto object-cover"
-            />
-          </div>
-        </div> */}
-      </div>
-
-      <div className="contain">
-        {/* <h2>Experience The Best Cafe In Bangalore</h2> */}
-      </div>
-
-      <div className="text-center md:py-20 py-10 px-10 md:mb-4 mb-10">
-        <h1 className="md:text-8xl text-4xl md:mb-6 mb-4 md:leading-26 leading-14 ">
+      <div className=" text-center md:py-10 py-10 px-10 md:mb-4 mb-10">
+        <h1 className="md:text-5xl text-4xl md:mb-2 mb-4 md:leading-26 leading-14 font-playfair-display">
           YOUR EUROPE TRIP IS <span className="">CONFIRMED !</span>{" "}
         </h1>
         <h2 className="text-2xl text-dolci leading-8">
@@ -53,11 +34,16 @@ const Hero = () => {
         </h2>
       </div>
 
-      <div className="passport flex justify-center mb-20 md:mt-[-8rem] mt-[-4rem]">
-        <img
+      <div className="w-full overflow-hidden  flex justify-center mb-20 md:mt-[-24rem] mt-[-4rem]">
+        {/* <img
           src="https://res.cloudinary.com/dq7bzydqz/image/upload/v1762952654/Dolci_Passport_Cover_Individual_Frame_ykgrcp.webp"
           alt=""
           className="md:w-[70%] w-full"
+        /> */}
+        <img
+          src="https://res.cloudinary.com/dq7bzydqz/image/upload/v1763465271/Dolci_Landing_Page_Section_copy_3_cpzb39.svg"
+          alt=""
+          className="md:w-[98%] w-300 "
         />
       </div>
 
@@ -135,21 +121,21 @@ const OnePassport = () => {
           <div className="w-full overflow-hidden grid grid-cols-2 gap-2 md:p-4 p-6">
             <div className="w-full md:h-80  col-span-2 overflow-hidden rounded-xl">
               <img
-                src="https://res.cloudinary.com/dq7bzydqz/image/upload/v1763048025/skyline-of-paris-france-2024-10-22-15-46-07-utc_11zon_jui4li.webp"
+                src="https://res.cloudinary.com/dq7bzydqz/image/upload/v1763391286/Gemini_Generated_Image_keqjc5keqjc5keqj_fzawhq.webp"
                 alt=""
-                className="w-full h-full object-cover object-bottom"
+                className="w-full h-full object-cover object-center transform scale-[1.2]"
               />
             </div>
             <div className="w-full h-60 object-cover overflow-hidden rounded-xl">
               <img
-                src="https://res.cloudinary.com/dq7bzydqz/image/upload/v1763045731/view-of-the-plaza-de-las-tendillas-and-the-sculpt-2025-10-06-23-11-54-utc_11zon_u5m4xr.webp"
+                src="https://res.cloudinary.com/dq7bzydqz/image/upload/v1763391285/Gemini_Generated_Image_va827lva827lva82_1_s0a1kq.webp"
                 alt=""
                 className=" w-full h-full object-cover"
               />
             </div>
             <div className="w-full h-60 object-cover overflow-hidden rounded-xl">
               <img
-                src="https://res.cloudinary.com/dq7bzydqz/image/upload/v1763045044/city-view-of-grand-canal-2025-02-22-17-06-29-utc_11zon_vxuhje.webp"
+                src="https://res.cloudinary.com/dq7bzydqz/image/upload/v1763391285/Gemini_Generated_Image_13vpkx13vpkx13vp_yxvlha.webp"
                 alt=""
                 className=" w-full h-full object-cover"
               />
@@ -165,58 +151,68 @@ const GetStarted = () => {
   return (
     <>
       <div id="getstarted" className="contain md:py-20">
-        <div className="max-w-[920px] mx-auto">
+        <div className="max-w-[650px] mx-auto">
           <div className="px-10 mt-8 text-center mb-12">
             <h2 className="text-4xl leading-12 ">How to get started?</h2>
           </div>
 
-          <div className="text-center mb-10 flex flex-col md:flex-row justify-center items-center">
-            <span className="w-10 h-10 flex justify-center items-center bg-dolci-blue rounded-full text-2xl mr-4">
-              1.{" "}
-            </span>
-            <p className="md:text-3xl text-2xl text-gray-600 leading-8">
-              {" "}
-              <i>Click below and enter your Bangalore address.</i>
-            </p>
-          </div>
+          <div className="">
+            <div className="flex items-center mb-10">
+              <div className="w-10 h-10 flex justify-center items-center bg-dolci-blue rounded-full text-xl mr-4">
+                1.{" "}
+              </div>
 
-          <div className="flex justify-center mb-4 mt-[-2rem]">
-            <img
-              src="https://res.cloudinary.com/dq7bzydqz/image/upload/v1763017293/Switch_Collage_LP_44_imgbor.png"
-              alt=""
-              className="md:w-50 w-30"
-            />
-          </div>
+              <div className="w-full">
+                <p className="md:text-2xl text-2xl text-gray-600 leading-8">
+                  {" "}
+                  <i>Click below and enter your Bangalore address.</i>
+                </p>
+              </div>
+            </div>
 
-          <div className="flex flex-col items-center justify-center mb-5">
-            <input
-              type="text"
-              className="input border-1 p-4 border-[#56a4d1] md:w-[60%] w-[80%] rounded-2xl mb-4"
-              placeholder="Name"
-            />
-            <input
-              type="text"
-              className="input border-1 p-4 border-[#56a4d1]  md:w-[60%] w-[80%] rounded-2xl mb-4"
-              placeholder="Address"
-            />
-            {/* <input type="text" className="input border-1 p-4 border-dolci-blue w-[60%] rounded-2xl mb-4" placeholder="city"/>
+            <div className="flex flex-col items-center justify-center mb-10">
+              <input
+                type="text"
+                className="input border-1 p-4 border-[#56a4d1] md:w-[60%] w-[80%] rounded-2xl mb-4"
+                placeholder="Name"
+              />
+              <input
+                type="text"
+                className="input border-1 p-4 border-[#56a4d1]  md:w-[60%] w-[80%] rounded-2xl mb-4"
+                placeholder="Address"
+              />
+              {/* <input type="text" className="input border-1 p-4 border-dolci-blue w-[60%] rounded-2xl mb-4" placeholder="city"/>
             <input type="text" className="input border-1 p-4 border-dolci-blue w-[60%] rounded-2xl mb-4" placeholder="state"/> */}
-            <input
-              type="number"
-              className="input border-1 p-4 border-[#56a4d1] md:w-[60%] w-[80%] rounded-2xl"
-              placeholder="Pincode"
-            />
-            <input type="text" className="Street" />
-            <button className="bg-dolci-blue md:w-[60%] w-[80%] p-4 rounded-2xl cursor-pointer text-xl font-bold">
-              {" "}
-              Claim My Europe Trip Today
-            </button>
+              <input
+                type="number"
+                className="input border-1 p-4 border-[#56a4d1] md:w-[60%] w-[80%] rounded-2xl"
+                placeholder="Pincode"
+              />
+              <input type="text" className="Street" />
+              <button className="bg-dolci-blue md:w-[60%] w-[80%] p-4 rounded-2xl cursor-pointer text-xl font-bold">
+                {" "}
+                Claim My Europe Trip Today
+              </button>
+            </div>
+
+            <div className="flex mb-10">
+              <div className="w-10 h-10 flex justify-center items-center bg-dolci-blue rounded-[100%] text-xl mr-4">
+                2.{" "}
+              </div>
+
+              <div className="w-full">
+                <p className="md:text-2xl text-2xl text-gray-600 leading-8">
+                  {" "}
+                  <i>
+                    Your Dolci Explorer Passport will be processed and delivered
+                    to your doorstep within 48 hours.
+                  </i>
+                </p>
+              </div>
+            </div>
           </div>
 
-          <div className="text-center mb-10 flex justify-center items-center ">
-            {/* <span className="w-10 h-10 flex justify-center items-center bg-dolci-blue rounded-full text-2xl mr-4">
-              1.{" "}
-            </span> */}
+          {/* <div className="text-center mb-10 flex justify-center items-center ">
             <p className="text-xl text-gray-600 w-150 leading-6">
               {" "}
               <i>
@@ -224,9 +220,9 @@ const GetStarted = () => {
                 your doorstep within 48 hours.
               </i>
             </p>
-          </div>
+          </div> */}
 
-          <div className="mx-auto mt-[-4rem] mb-10">
+          {/* <div className="mx-auto mt-[-4rem] mb-10">
             <div className="passport flex justify-center ">
               <img
                 src="https://res.cloudinary.com/dq7bzydqz/image/upload/v1763036723/3_ejnwcx.webp"
@@ -234,31 +230,40 @@ const GetStarted = () => {
                 className="w-[100%]"
               />
             </div>
-          </div>
-
-          <div className="p-8 md:p-0 grid md:grid-cols-6 grid-cols-3 gap-2 mt-[-5rem]">
-            {[
-              "https://res.cloudinary.com/dq7bzydqz/image/upload/v1762952231/Dolci_Passport_Stamp_Sticker_06_fzgaqa.webp",
-              "https://res.cloudinary.com/dq7bzydqz/image/upload/v1762952231/Dolci_Passport_Stamp_Sticker_07_bxriro.webp",
-              "https://res.cloudinary.com/dq7bzydqz/image/upload/v1762952231/Dolci_Passport_Stamp_Sticker_02_scikub.webp",
-              "https://res.cloudinary.com/dq7bzydqz/image/upload/v1762952230/Dolci_Passport_Stamp_Sticker_05_y4apop.webp",
-              "https://res.cloudinary.com/dq7bzydqz/image/upload/v1762952229/Dolci_Passport_Stamp_Sticker_04_wfu8au.webp",
-              "https://res.cloudinary.com/dq7bzydqz/image/upload/v1762952229/Dolci_Passport_Stamp_Sticker_09_jvywwo.webp",
-              ,
-            ].map((item) => (
-              <img src={item} alt="" className="w-full" />
-            ))}
-          </div>
+          </div> */}
+        </div>
+        <div className="p-8 md:p-0 md:py-12 grid md:grid-cols-6 grid-cols-3 gap-2 mt-[0rem]">
+          {[
+            "https://res.cloudinary.com/dq7bzydqz/image/upload/v1762952231/Dolci_Passport_Stamp_Sticker_06_fzgaqa.webp",
+            "https://res.cloudinary.com/dq7bzydqz/image/upload/v1762952231/Dolci_Passport_Stamp_Sticker_07_bxriro.webp",
+            "https://res.cloudinary.com/dq7bzydqz/image/upload/v1762952231/Dolci_Passport_Stamp_Sticker_02_scikub.webp",
+            "https://res.cloudinary.com/dq7bzydqz/image/upload/v1762952230/Dolci_Passport_Stamp_Sticker_05_y4apop.webp",
+            "https://res.cloudinary.com/dq7bzydqz/image/upload/v1762952229/Dolci_Passport_Stamp_Sticker_04_wfu8au.webp",
+            "https://res.cloudinary.com/dq7bzydqz/image/upload/v1762952229/Dolci_Passport_Stamp_Sticker_09_jvywwo.webp",
+            ,
+          ].map((item) => (
+            <img src={item} alt="" className="w-full" />
+          ))}
         </div>
       </div>
     </>
   );
 };
 
+{
+  /* <div className="flex justify-center mb-4 mt-[-2rem]">
+            <img
+              src="https://res.cloudinary.com/dq7bzydqz/image/upload/v1763017293/Switch_Collage_LP_44_imgbor.png"
+              alt=""
+              className="md:w-50 w-30"
+            />
+          </div> */
+}
+
 const ArriveAtDolci = () => {
   return (
     <>
-      <div className="contain py-20">
+      <div className="contain">
         <div className="max-w-[920px] mx-auto px-6 md:px-0">
           <div className=" mt-8 text-center mb-12">
             <h2 className="text-3xl md:text-4xl leading-12 ">
@@ -432,7 +437,7 @@ const Home = () => {
 
 const Landing = () => {
   return (
-    <main className="dolci_system_design relative">
+    <main className="dolci_landing relative">
       <Home />
       {/* <Test /> */}
     </main>
